c883eaaebce0972ffe997298653d067ecf774bcc
pavelbekarev <bekarev.ps@gmail.com>
2024-09-08 19:48:15 +0500

    adding dist subtree

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/bazzi2/" : "/",
   plugins: [
     vue(),
   ],
